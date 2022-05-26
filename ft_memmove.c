626f9c3f7a7adbb20c58195e19625a8cc17fde27
jbillet <jbillet@student.42sp.org.br>
2022-05-26 06:09:02 +0200

    Libft 42

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jbillet <jbillet@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 00:07:05 by jbillet           #+#    #+#             */
-/*   Updated: 2022/05/11 22:46:29 by jbillet          ###   ########.fr       */
+/*   Updated: 2022/05/26 05:56:50 by jbillet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,17 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t			i;
 	unsigned char	*dest_char;
 	unsigned char	*src_char;
-	unsigned char	*temp;
 
 	if (n < 1)
 		return (dest);
-	i = 0;
 	dest_char = (unsigned char *)dest;
 	src_char = (unsigned char *)src;
-	temp = (unsigned char *)ft_calloc(n, sizeof(char));
-	while (i < n)
-	{
-		temp[i] = src_char[i];
-		i++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		dest_char[i] = temp[i];
-		i++;
-	}
-	free(temp);
+	if (dest > src)
+		while (n--)
+			dest_char[n] = src_char[n];
+	else
+		ft_memcpy(dest_char, src_char, n);
 	return (dest);
 }
